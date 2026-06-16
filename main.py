5ae62ad688891ab1f90097de7bc6e50fbba532c0
arturlev24-ship-it <arturlev24@gmail.com>
2026-06-16 15:50:31 +0700

    main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import aiosqlite
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-BOT_TOKEN = "BOT_TOKEN"
+BOT_TOKEN = "TELEGRAM_BOT_TOKEN"
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
