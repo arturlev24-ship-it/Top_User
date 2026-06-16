40f1eb1fd63f9f3fc088119dfb89b0aa748328e3
arturlev24-ship-it <arturlev24@gmail.com>
2026-06-16 15:44:44 +0700

    main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import aiosqlite
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-BOT_TOKEN = "YOUR_BOT_TOKEN_HERE"
+BOT_TOKEN = "BOT_TOKEN"
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
